8f4fdaba60dd066a26b1b05b0f3ec20eca47f09f
Gabe Durazo <gabe@durazo.us>
2019-12-02 07:53:11 -0600

    feat: Day2

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,18 @@
 mod day1;
+mod day2;
 
 fn main() {
-    day1::puzzle1();
-    day1::puzzle2();
+    let day = 2;
+
+    match day {
+        1 => {
+            day1::puzzle1();
+            day1::puzzle2();
+        }
+        2 => {
+            day2::puzzle1();
+            day2::puzzle2();
+        }
+        _ => unreachable!(),
+    }
 }
